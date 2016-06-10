b2a5012a49808b443cbafc4f19ebe457d8737e96
Camden Cheek <ccheek@Camdens-MacBook-Air.local>
2016-06-10 00:44:28 -0400

    Combined some zsh files, vim and neovim both working

--- zsh/zsh-theme.zsh
+++ zsh/zsh-theme.zsh
@@ -1,3 +1,33 @@
+parse_git_dirty () {
+	local STATUS=''
+	local FLAGS
+	FLAGS=('--porcelain')
+	if [[ "$(command git config --get oh-my-zsh.hide-dirty)" != "1" ]]
+	then
+		if [[ $POST_1_7_2_GIT -gt 0 ]]
+		then
+			FLAGS+='--ignore-submodules=dirty'
+		fi
+		if [[ "$DISABLE_UNTRACKED_FILES_DIRTY" = "true" ]]
+		then
+			FLAGS+='--untracked-files=no'
+		fi
+		STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
+	fi
+	if [[ -n $STATUS ]]
+	then
+		echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+	else
+		echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+	fi
+}
+
+git_prompt_info () {
+	local ref
+	ref=$(command git symbolic-ref HEAD 2> /dev/null)  || ref=$(command git rev-parse --short HEAD 2> /dev/null)  || return 0
+	echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)"
+}
+
 source "$HOME/.dotfiles/zsh/zsh-git-integration.zsh"
 setopt prompt_subst
 function box_name {
