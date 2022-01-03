9ad1dc64a48979136987d6c7d9475b452ddfb7ec
Camden Cheek <camden@ccheek.com>
2022-01-02 17:07:17 -0700

    some formatting cleanup

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,40 +1,16 @@
-
 -- App vars
-local browser = "Firefox Developer Edition"
-local terminal = "kitty"
-local slack = "Slack"
-local chat = "Telegram"
-local notes = "Boostnote"
+local hs
 
 -- variable config
 hs.window.animationDuration = 0
 hs.window.setShadows(false)
 
--- extensions, available in hammerspoon console
-ext = {
-  frame    = {},
-  win      = {},
-  app      = {}, 
-  utils    = {},
-  cache    = {},
-  watchers = {}
-}
-
-
-startingLayout = {
-    {terminal, nil, hs.screen.find(724852372), hs.layout.right50, nil, nil},
-    {browser, nil, hs.screen.find(724852372), hs.layout.left50, nil, nil},
-    {slack, nil, hs.screen.find(69733378), hs.layout.maximized, nil, nil},
-    {"Code", nil, hs.screen.find(724852499), hs.layout.maximized, nil, nil},
-}
-
-
-tileLeft = function(win) 
+local tileLeft = function(win)
     local f = win:frame()
     local newf = win:frame()
     local screen = win:screen()
     local max = screen:frame()
-    
+
     newf.x = max.x
     newf.y = max.y
     newf.w = max.w / 2
@@ -48,12 +24,12 @@ tileLeft = function(win)
     end
 end
 
-tileRight = function(win) 
+local tileRight = function(win)
     local f = win:frame()
     local newf = win:frame()
     local screen = win:screen()
     local max = screen:frame()
-    
+
     newf.x = max.x + (max.w / 2)
     newf.y = max.y
     newf.w = max.w / 2
@@ -67,29 +43,27 @@ tileRight = function(win)
     end
 end
 
-tileUp = function(win) 
-    local f = win:frame()
+local tileUp = function(win)
     local newf = win:frame()
     local screen = win:screen()
     local max = screen:frame()
-    
+
     newf.x = max.x
     newf.y = max.y
-    newf.w = max.w 
+    newf.w = max.w
     newf.h = max.h / 2
 
     win:setFrame(newf, 0)
 end
 
-tileDown = function(win) 
-    local f = win:frame()
+local tileDown = function(win)
     local newf = win:frame()
     local screen = win:screen()
     local max = screen:frame()
-    
+
     newf.x = max.x
     newf.y = max.y + (max.h / 2)
-    newf.w = max.w 
+    newf.w = max.w
     newf.h = max.h / 2
 
     win:setFrame(newf, 0)
@@ -98,30 +72,18 @@ end
 -- Tile down binding
 hs.hotkey.bind("alt", "j", function()
   local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
   tileDown(win)
 end)
 
 -- Tile up binding
 hs.hotkey.bind("alt", "k", function()
   local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
   tileUp(win)
 end)
 
 -- Tile right binding
 hs.hotkey.bind("alt", "l", function()
   local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
   if not tileRight(win) then
       win:moveOneScreenEast()
       tileLeft(win)
@@ -131,10 +93,6 @@ end)
 -- Tile right binding
 hs.hotkey.bind("alt", "h", function()
   local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
   if not tileLeft(win) then
       win:moveOneScreenWest()
       tileRight(win)
@@ -153,18 +111,23 @@ hs.hotkey.bind("alt", "m", function()
 
   f.x = max.x
   f.y = max.y
-  f.w = max.w 
+  f.w = max.w
   f.h = max.h
   win:setFrame(f, 0.01)
 end)
 
 
 -- App hotkeys
+local browser = "Firefox Developer Edition"
+local terminal = "kitty"
+local slack = "Slack"
+local chat = "Telegram"
+local music = "Spotify"
 hs.fnutils.each({
     { key = "a", app = browser },
     { key = "d", app = chat },
     { key = "s", app = slack },
-    { key = "f", app = "Spotify" },
+    { key = "f", app = music },
     { key = "space", app = terminal }
 }, function(object)
     hs.hotkey.bind("alt", object.key, function() hs.application.launchOrFocus(object.app) end)
