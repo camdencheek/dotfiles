4967dd7e22fa381d61be13b939fcc9ccd95359d2
Camden Cheek <camden@ccheek.com>
2024-01-13 20:54:18 -0700

    even more simplification

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,86 +1,86 @@
 local M = {}
 
 function M.config()
-	local actions = require("telescope.actions")
-	local sorters = require("telescope.sorters")
-	require("telescope").setup({
-		defaults = {
-			prompt_prefix = "❯ ",
-			selection_caret = "❯ ",
+    local actions = require("telescope.actions")
+    local sorters = require("telescope.sorters")
+    require("telescope").setup({
+        defaults = {
+            prompt_prefix = "❯ ",
+            selection_caret = "❯ ",
 
-			mappings = {
-				i = {
-					["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-					["<esc>"] = actions.close,
-					["<C-c>"] = actions.close,
-				},
-			},
+            mappings = {
+                i = {
+                    ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+                    ["<esc>"] = actions.close,
+                    ["<C-c>"] = actions.close,
+                },
+            },
 
-			file_sorter = sorters.get_fzy_sorter,
-			file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-			grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-			qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-		},
-		extensions = {
-			["zf-native"] = {
-				-- options for sorting file-like items
-				file = {
-					-- override default telescope file sorter
-					enable = true,
+            file_sorter = sorters.get_fzy_sorter,
+            file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+            grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+            qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+        },
+        extensions = {
+            ["zf-native"] = {
+                -- options for sorting file-like items
+                file = {
+                    -- override default telescope file sorter
+                    enable = true,
 
-					-- highlight matching text in results
-					highlight_results = true,
+                    -- highlight matching text in results
+                    highlight_results = true,
 
-					-- enable zf filename match priority
-					match_filename = true,
-				},
-			},
-		},
-	})
-	require("telescope").load_extension("fzy_native")
-	-- require("telescope").load_extension("luasnip")
-	require("telescope").load_extension("zf-native")
+                    -- enable zf filename match priority
+                    match_filename = true,
+                },
+            },
+        },
+    })
+    require("telescope").load_extension("fzy_native")
+    -- require("telescope").load_extension("luasnip")
+    require("telescope").load_extension("zf-native")
 
-	local builtin = require("telescope.builtin")
+    local builtin = require("telescope.builtin")
 
-	vim.keymap.set("n", "<leader>ff", builtin.find_files)
-	vim.keymap.set("n", "<leader>ft", builtin.git_files)
-	vim.keymap.set("n", "<leader>fg", builtin.live_grep)
-	vim.keymap.set("n", "<leader>fo", builtin.oldfiles)
-	vim.keymap.set("n", "<leader>fd", builtin.fd)
-	vim.keymap.set("n", "<leader>fb", builtin.buffers)
-	vim.keymap.set("c", "<C-r>", builtin.command_history)
-	vim.keymap.set("n", "<leader>ga", "lsp_code_actions")
+    vim.keymap.set("n", "<leader>ff", builtin.find_files)
+    vim.keymap.set("n", "<leader>ft", builtin.git_files)
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+    vim.keymap.set("n", "<leader>fo", builtin.oldfiles)
+    vim.keymap.set("n", "<leader>fd", builtin.fd)
+    vim.keymap.set("n", "<leader>fb", builtin.buffers)
+    vim.keymap.set("c", "<C-r>", builtin.command_history)
+    vim.keymap.set("n", "<leader>ga", "lsp_code_actions")
 
-	-- Telescope Meta
-	vim.keymap.set("n", "<leader>fB", "builtin")
+    -- Telescope Meta
+    vim.keymap.set("n", "<leader>fB", "builtin")
 
-	local function fd_nvim()
-		return require("telescope.builtin")["fd"]({
-			prompt_title = "NVIM Config",
-			cwd = "~/.config/nvim",
-		})
-	end
+    local function fd_nvim()
+        return builtin.fd({
+            prompt_title = "NVIM Config",
+            cwd = "~/.config/nvim",
+        })
+    end
 
-	local function fn_nvim()
-		require("telescope.builtin")["fd"]({
-			prompt_title = "Notes",
-			cwd = "~/notes",
-			find_command = { "fd", "--type", "f", "--strip-cwd-prefix", "--glob", "*.norg" },
-		})
-		vim.cmd("cd ~/notes")
-	end
+    local function fn_nvim()
+        builtin.fd({
+            prompt_title = "Notes",
+            cwd = "~/notes",
+            find_command = { "fd", "--type", "f", "--strip-cwd-prefix", "--glob", "*.norg" },
+        })
+        vim.cmd("cd ~/notes")
+    end
 
-	local function fm_nvim()
-		return require("telescope.builtin")["live_grep"]({
-			prompt_title = "Notes Grep",
-			cwd = "~/notes",
-		})
-	end
+    local function fm_nvim()
+        return builtin.live_grep({
+            prompt_title = "Notes Grep",
+            cwd = "~/notes",
+        })
+    end
 
-	vim.keymap.set("n", "<leader>fc", fd_nvim)
-	vim.keymap.set("n", "<leader>fn", fn_nvim)
-	vim.keymap.set("n", "<leader>fm", fm_nvim)
+    vim.keymap.set("n", "<leader>fc", fd_nvim)
+    vim.keymap.set("n", "<leader>fn", fn_nvim)
+    vim.keymap.set("n", "<leader>fm", fm_nvim)
 end
 
 return M
